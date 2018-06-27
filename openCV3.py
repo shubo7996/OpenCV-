4ddf0bf97615eb1a671803606cad9264c3f7438b
shubo7996 <whatsupmofo420@gmail.com>
2018-06-27 23:53:21 +0530

    Draw and Write

--- openCV3.py
+++ openCV3.py
@@ -11,6 +11,8 @@ pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
 #pts = pts.reshape((-1,-1,2))
 cv2.polylines(img, [pts], True, (0,255,255), 3)
 
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(img, 'ovenCV3', (500,450), font, 1, (200,255,255), 2, cv2.LINE_AA)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
