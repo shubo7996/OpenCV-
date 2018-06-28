6faff31e057dcbe70d4337ba5e737991b959b503
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 18:24:52 +0530

    Feature Matching

---
+++ openCV13.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread('opencv-feature-matching-template.jpg',0)
+img2 = cv2.imread('opencv-feature-matching-image.jpg',0)
