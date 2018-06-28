030d750516a596a35e2a03bb96fea455eb64f8b5
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 21:26:13 +0530

    face and eye Detection

--- openCV15.py
+++ openCV15.py
@@ -11,7 +11,7 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x+w, y+h), (255,0,0), 2)
+        cv2.rectangle(img, (x,y) ,(x+w, y+h), (255,0,0), 2)
         roi_gray = gray[y: y+h, x: x+w]
         roi_color = img[y: y+h, x: x+w] 
         eyes = eye_cascade.detectMultiScale(roi_gray)
