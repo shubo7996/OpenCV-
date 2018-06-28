1299932bdd0cd2407dcb540aede1cbe9ce052020
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 12:47:49 +0530

    color filter

--- openCV7.py
+++ openCV7.py
@@ -7,8 +7,9 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0,0,0])
-    upper_red = np.array([255,255,255])
+    lower_red = np.array([150,150,50])
+    upper_red = np.array([180,255,150
+    ])
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask = mask)
