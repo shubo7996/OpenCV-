da1b1e8fd4d4d3f3af44cfc16e29c407911232c9
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 12:57:10 +0530

    color filter blur

--- openCV7.py
+++ openCV7.py
@@ -7,6 +7,7 @@ while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    #hsv hue sat value
     lower_red = np.array([150,150,50])
     upper_red = np.array([180,255,150
     ])
@@ -14,9 +15,16 @@ while True:
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
+    kernel = np.ones((15,15), np.float32)/255
+    smoothed = cv2.filter2D(res, -1, kernel)
+    blur = cv2.GaussianBlur(res, (15,15), 0)
+    
+    
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
+    #cv2.imshow('mask', mask)
     cv2.imshow('res', res)
+    #cv2.imshow('smoothed', smoothed)
+    cv2.imshow('blur', blur)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
