875da86a56d87d30b5637dad1c3d76e2b9a14ba5
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 17:34:59 +0530

    Edge Detection and Gradiance

--- openCV9.py
+++ openCV9.py
@@ -9,14 +9,15 @@ while True:
     laplacian = cv2.Laplacian(frame,cv2.CV_64F)
     sobelx = cv2.Sobel(frame,cv2.CV_64F, 1, 0, ksize= 5)
     sobely = cv2.Sobel(frame,cv2.CV_64F, 0, 1, ksize= 5)
-
+    edges = cv2.Canny(frame, 100, 200)
 
 
     cv2.imshow('original', frame)
     cv2.imshow('laplacian',laplacian)
     cv2.imshow('sobelx', sobelx)
     cv2.imshow('sobely',sobely)
-  
+    cv2.imshow('edges',edges)
+
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
