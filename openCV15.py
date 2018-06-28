80a62be78473fe5e81861aa2dd0e5b3334963f09
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 21:28:36 +0530

    face and eye Detection

--- openCV15.py
+++ openCV15.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
