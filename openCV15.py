ae4cf6d309347538cd65d026cb80649c79efaaa3
shubo7996 <whatsupmofo420@gmail.com>
2018-06-28 22:19:29 +0530

    face and eye Detection

--- openCV15.py
+++ openCV15.py
@@ -21,7 +21,7 @@ while True:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0), 2)
         
         cv2.imshow('img', img)
-        k = cv2.waitKey(30) & 0xff
+        k = cv2.waitKey(30) & 0xFF
         if k == 27:
             break
 
